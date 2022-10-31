668b5eb50a6948c349b27383716bfe65e7395bd3
Sviirina <vozilkina@inbox.ru>
2022-10-31 19:45:33 +0700

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
 
+        Author joanneRowling  = new Author ("Joanne", "Rowling");
+        Book harryPotter = new Book("Harry Potter", joanneRowling, 1997);
+        Book theIdiot = new Book("The Idiot", new Author("Fyodor","Dostoyevsky"), 1868);
+        harryPotter.setYear(2000);
+        System.out.println(harryPotter.getYear());
     }
 }
